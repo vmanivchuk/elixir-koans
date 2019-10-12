67198dcceed85a8de136d651bf8cd636ead31984
Felipe Sere <felipesere@gmail.com>
2019-10-12 12:03:27 +0200

    Remove stacktrace warning

--- lib/execute.ex
+++ lib/execute.ex
@@ -40,7 +40,7 @@ defmodule Execute do
 
   defp expand(error, module) do
     {file, line} =
-      System.stacktrace()
+      :erlang.get_stacktrace()
       |> Enum.drop_while(&(!in_koan?(&1, module)))
       |> List.first()
       |> extract_file_and_line
